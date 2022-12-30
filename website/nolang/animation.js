fa1055561cdb95aa08ec561621b5033071a56a3a
cmine20 <38400272+cmine20@users.noreply.github.com>
2022-12-30 19:40:19 +0800

    Update animation.js

--- website/nolang/animation.js
+++ website/nolang/animation.js
@@ -1,3 +1,5 @@
+//Animation from Left to Right Entry
+
 const observer = new IntersectionObserver((entries) => {
     entries.forEach((entry) => {
       console.log(entry)
@@ -10,3 +12,24 @@ const observer = new IntersectionObserver((entries) => {
   });
   const hiddenElements = document.querySelectorAll('.hidden');
   hiddenElements.forEach((el) => observer.observe(el));
+
+  //loading Screen
+
+  window.addEventListener("load", function () {
+    setTimeout(function () {
+      document.getElementById("loading-screen").style.display = "none";
+    }, 5000); // 5 seconds
+  });
+
+  //Jumping Word Effects
+
+const letters = document.querySelectorAll("#loading-screen p span");
+
+let i = 0;
+setInterval(function() {
+  letters.forEach(function(letter) {
+    letter.style.top = Math.sin(i + letter.offsetLeft / 100) * 20 + "px";
+    letter.style.left = Math.cos(i + letter.offsetTop / 100) * 20 + "px";
+  });
+  i += 0.1;
+}, 50);
